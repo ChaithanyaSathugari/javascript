b9e8f59a7a2cf4c1cd5b3deb1e8f9556a7d7a8f2
chaithanya <chaithanyareddy9848@gmail.com>
2023-09-22 15:52:22 +0530

    first commit

---
+++ m.js
@@ -0,0 +1,13 @@
+function switchOn(){
+    document.getElementById("switchStatus").textContent="Switched On"
+    document.getElementById("bulbImage").src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-on-img.png";
+    document.getElementById("catImage"). src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/cat-img.png";
+    
+}
+function switchOff(){
+    document.getElementById("switchStatus").textContent="Switched Off"
+    document.getElementById("bulbImage").src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-off-img.png";
+    document.getElementById("catImage").src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/cat-eyes-img.png";
+
+
+}
